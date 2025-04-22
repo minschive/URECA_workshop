0810f3d3265a78f0129b6b86a15fb96ff6a505df
Jimin Byun <fraise3838@naver.com>
2025-04-22 15:17:59 +0900

    Remove unused project folders

--- PhoneSpringBootMVCDBAjaxMybatis/src/main/java/com/example/phonespringbootmvcdbajaxmybatis/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.mycom.myapp.user.service;
-
-import com.mycom.myapp.user.dto.UserDto;
-import com.mycom.myapp.user.dto.UserResultDto;
-
-// UserResultDto 만들어서 리턴
-public interface UserService {
-    UserResultDto registerUser(UserDto userDto);
-}
